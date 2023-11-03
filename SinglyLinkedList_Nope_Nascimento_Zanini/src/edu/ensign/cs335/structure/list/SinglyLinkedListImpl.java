0064b8ec429a16fcbeb27969c841f7b4609682e2
JNope <91224053+JNope01@users.noreply.github.com>
2023-11-03 13:29:19 -0600

    Ready to Code!

---
+++ SinglyLinkedList_Nope_Nascimento_Zanini/src/edu/ensign/cs335/structure/list/SinglyLinkedListImpl.java
@@ -0,0 +1,83 @@
+package edu.ensign.cs335.structure.list;
+
+public class SinglyLinkedListImpl implements SinglyLinkedList<Integer> {
+
+	@Override
+	public void append(Integer data) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int getSize() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void insert(Integer data) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void insertNth(Integer data, int index) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void insertHead(Integer data) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean search(Integer data) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Integer getNth(int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean delete() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean deleteHead() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean deleteNth(int index) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+
+	}
+
+}

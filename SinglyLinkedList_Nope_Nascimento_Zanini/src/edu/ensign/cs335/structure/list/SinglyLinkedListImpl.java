9f46e923282c729a18ae8f17be707192151a7e23
JNope <91224053+JNope01@users.noreply.github.com>
2023-11-03 19:35:10 -0600

    done

--- SinglyLinkedList_Nope_Nascimento_Zanini/src/edu/ensign/cs335/structure/list/SinglyLinkedListImpl.java
+++ SinglyLinkedList_Nope_Nascimento_Zanini/src/edu/ensign/cs335/structure/list/SinglyLinkedListImpl.java
@@ -2,42 +2,40 @@ package edu.ensign.cs335.structure.list;
 
 
 
-public class SinglyLinkedListImpl implements SinglyLinkedList<Integer> {
-	private Node head;
+public class SinglyLinkedListImpl<T> implements SinglyLinkedList<T> {
+	private Node<T> head;
 
 	@Override
-	public void append(Integer data) {
-		// TODO Auto-generated method stub
-		
+	public void append(T data) {
+		Node<T> tmp = head;
+		while (tmp.getNext()!= null) {
+			tmp = tmp.getNext();
+		}
+		Node<T> inTemp = new Node<T>(data);
+		tmp.setNext(inTemp);
 	}
 
 	@Override
 	public int getSize() {
-		int size = 1;
-		Node tmp = head;
-		try{
-			while (tmp.getNext()!= null) {
+		int size = 0;
+		Node<T> tmp = head;
+		while (tmp!= null) {
 				size++;
 				tmp = tmp.getNext();
-			}
-		} catch (Exception e) {
-			size = 0;
 		}
 		return size;
 	}
 
 	@Override
 	public int getCount() {
-		int count = 1;
-		Node tmp = head;
-		try {
-		while (tmp.getNext()!= null) {
+		int count = 0;
+		Node<T> tmp = head;
+		
+		while (tmp!= null) {
 			if (tmp.getData()!=null) {
 				count++;
 			}
 			tmp = tmp.getNext();
-		}}catch(Exception e) {
-			count = 0;
 		}
 		return count;
 	}
@@ -54,64 +52,62 @@ public class SinglyLinkedListImpl implements SinglyLinkedList<Integer> {
 	}
 
 	@Override
-	public void insert(Integer data) {
-		Node tmp = head;
+	public void insert(T data) {
+		Node<T> tmp = head;
 		while (tmp.getNext()!= null) {
 			tmp = tmp.getNext();
 		}
-		Node intemp = new Node(data);
+		Node<T> intemp = new Node<T>(data);
 		tmp.setNext(intemp);
 	}
 
 	@Override
-	public void insertNth(Integer data, int index) {
-		Node tmp = head;
+	public void insertNth(T data, int index) {
+		Node<T> tmp = head;
 		while (--index>0) {
 			tmp=tmp.getNext();
 		}
-		Node tmp2 =tmp.getNext();
-		Node intemp = new Node(data);
+		Node<T> tmp2 =tmp.getNext();
+		Node<T> intemp = new Node<T>(data);
 		tmp.setNext(intemp);
 		intemp.setNext(tmp2);
 	}
 
 	@Override
-	public void insertHead(Integer data) {
-		Node tmp = head;
-		head = new Node(data);
+	public void insertHead(T data) {
+		Node<T> tmp = head;
+		head = new Node<T>(data);
 		head.setNext(tmp);
 	}
 
 	@Override
-	public boolean search(Integer data) {
+	public boolean search(T data) {
 		boolean val = false;
-		Node tmp = head;
-		do {
+		Node<T> tmp = head;
+		while(tmp!=null) {
 			if ((Integer)tmp.getData()==data) {
-				
-				System.out.println(tmp.getData());
-				System.out.println(data);
 				val=true;
 				break;
 			}
 			tmp=tmp.getNext();
-		} while(tmp.getNext()!=null);
+		}
 		return val;
 	}
 
 	@Override
-	public Integer getNth(int index) {
-		Node tmp = head;
-		while (--index>0) {
+	public T getNth(int index) {
+		Node<T> tmp = head;
+		while (index>0) {
 			tmp=tmp.getNext();
+			index--;
 		}
-		return (Integer)tmp.getData();
+		return (T)tmp.getData();
 	}
 
 	@Override
 	public boolean delete() {
 		int size=getSize();
-		Node tmp = head;
+		Node<T> tmp = head;
 		while (--size>1) {
 			tmp=tmp.getNext();
 		}
@@ -127,7 +123,7 @@ public class SinglyLinkedListImpl implements SinglyLinkedList<Integer> {
 
 	@Override
 	public boolean deleteNth(int index) {
-		Node tmp = head;
+		Node<T> tmp = head;
 		while (--index>1) {
 			tmp=tmp.getNext();
 		}
@@ -140,20 +136,19 @@ public class SinglyLinkedListImpl implements SinglyLinkedList<Integer> {
 	
 	@Override
 	public String toString() {
-		Node tmp = head;
+		Node<T> tmp = head;
 		String cont = "";
-		int i = 0;
 		while(tmp.getNext()!=null) {
 			cont=cont+tmp.getData()+" -> ";
 			tmp=tmp.getNext();
 		}
-		cont=cont+tmp.getData();
+		cont = cont + tmp.getData();
 		return cont;
 	}
 
 	@Override
 	public void clear() {
-		head.setNext(null);
+		head=null;
 	}
 
 }
